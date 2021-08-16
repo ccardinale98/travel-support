7cf91edb31461df02feda938200f0794cd5244c6
Jason Park <jihoonswdev@gmail.com>
2021-08-16 14:12:34 -0500

    update

--- assets/script/exchange.js
+++ assets/script/exchange.js
@@ -1,1 +1,18 @@
-console.log("new file");
+var requestUrl =
+  "https://v6.exchangerate-api.com/v6/f476b9db153c54bad3c7b6e4/latest/USD";
+var responseText = document.getElementById("response-text");
+var fetchButton = document.getElementById("fetch-button");
+
+function getApi(requestUrl) {
+  fetch(requestUrl).then(function (response) {
+    console.log(response);
+    if (response.status === 200) {
+      responseText.textContent = response.status;
+    }
+    return response.json();
+  });
+}
+
+getApi(requestUrl);
+
+// fetchButton.addEventListener("click", getApi);
