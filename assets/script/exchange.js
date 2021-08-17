061d78528d9820511a61f96e0d8a3e71fee0589e
Jason Park <jihoonswdev@gmail.com>
2021-08-17 15:11:07 -0500

    jihoon-branch

--- assets/script/exchange.js
+++ assets/script/exchange.js
@@ -1,20 +1,36 @@
-// var requestUrl =
-//   "https://v6.exchangerate-api.com/v6/f476b9db153c54bad3c7b6e4/latest/USD";
-// var responseText = document.getElementById("btn-exchange");
-// var fetchButton = document.getElementById("fetch-button");
+var select = document.querySelectorAll("select");
+var input = document.querySelectorAll("input");
+var requestUrl =
+  "http://api.exchangeratesapi.io/v1/latest?access_key=af421db273008dfe472d3260a6a0b4e2";
 
+async function getCurrency() {
+  var res = await fetch(requestUrl);
+  var data = await res.json();
 
-// function getApi(requestUrl) {
-//   fetch(requestUrl).then(function (response) {
-//     console.log(response);
-//     if (response.status === 200) {
-//         btnExchange.textContent = response.status;D
-//     }
-//     return response.json();
-//   });
-// }
+  var arrKeys = Object.keys(data.rates);
+  var rates = data.rates;
+  let html = "";
 
-// getApi(requestUrl);
+  console.log(rates);
+  arrKeys.map((item) => {
+    return (html += `<option value=${item}>${item}</option>`);
+  });
 
-// fetchButton.addEventListener("click", getApi);
+  for (let i = 0; i < select.length; i++) {
+    select[i].innerHTML = html;
+  }
 
+  function convert(i, j) {
+    input[i].value =
+      (input[j].value * rates[select[i].value]) / rates[select[j].value];
+  }
+
+  input[0].addEventListener("keyup", () => convert(1, 0));
+
+  input[1].addEventListener("keyup", () => convert(0, 1));
+
+  select[0].addEventListener("change", () => convert(1, 0));
+
+  select[1].addEventListener("change", () => convert(0, 1));
+}
+getCurrency();
