854fcd6cdbda8136048c825e99b8a238c3d065e7
Jason Park <jihoonswdev@gmail.com>
2021-08-17 13:47:30 -0500

    update

--- assets/script/exchange.js
+++ assets/script/exchange.js
@@ -1,18 +1,20 @@
-var requestUrl =
-  "https://v6.exchangerate-api.com/v6/f476b9db153c54bad3c7b6e4/latest/USD";
-var responseText = document.getElementById("response-text");
-var fetchButton = document.getElementById("fetch-button");
+// var requestUrl =
+//   "https://v6.exchangerate-api.com/v6/f476b9db153c54bad3c7b6e4/latest/USD";
+// var responseText = document.getElementById("btn-exchange");
+// var fetchButton = document.getElementById("fetch-button");
 
-function getApi(requestUrl) {
-  fetch(requestUrl).then(function (response) {
-    console.log(response);
-    if (response.status === 200) {
-      responseText.textContent = response.status;
-    }
-    return response.json();
-  }); 
-}
 
-getApi(requestUrl);
+// function getApi(requestUrl) {
+//   fetch(requestUrl).then(function (response) {
+//     console.log(response);
+//     if (response.status === 200) {
+//         btnExchange.textContent = response.status;D
+//     }
+//     return response.json();
+//   });
+// }
+
+// getApi(requestUrl);
 
 // fetchButton.addEventListener("click", getApi);
+
