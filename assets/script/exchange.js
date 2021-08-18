226d69df7d22f581dcf07c539ec8faf6e98b3f27
Jason Park <jihoonswdev@gmail.com>
2021-08-18 14:59:04 -0500

    update

--- assets/script/exchange.js
+++ assets/script/exchange.js
@@ -1,5 +1,6 @@
 var select = document.querySelectorAll("select");
-var input = document.querySelectorAll("input");
+var input = document.querySelectorAll(".exchange-input");
+
 var requestUrl =
   "http://api.exchangeratesapi.io/v1/latest?access_key=af421db273008dfe472d3260a6a0b4e2";
 
@@ -15,22 +16,30 @@ async function getCurrency() {
   arrKeys.map((item) => {
     return (html += `<option value=${item}>${item}</option>`);
   });
-
+ //   console.log(html);
   for (let i = 0; i < select.length; i++) {
     select[i].innerHTML = html;
   }
 
-  function convert(i, j) {
+  function convert(i, a) {
     input[i].value =
-      (input[j].value * rates[select[i].value]) / rates[select[j].value];
+    (input[a].value * rates[select[i].value]) / rates[select[a].value];
+    // var num = input[i].value
+    // var n = num.toFixed(2);
+    // document.getElementById("input").innerHTML = n;
   }
-
+//   console.log(rates[select[1].value]); //rates
   input[0].addEventListener("keyup", () => convert(1, 0));
-
+//     input[1].value = input[0].value * rates[select[1].value] / rates[select[0].value];
+  //   );
   input[1].addEventListener("keyup", () => convert(0, 1));
-
+//     input[0].value = input[1].value * rates[select[0].value] / rates[select[1].value];
+  //   });
   select[0].addEventListener("change", () => convert(1, 0));
-
+//     input[1].value = input[0].value * rates[select[1].value] / rates[select[0].value];
+  //   });
   select[1].addEventListener("change", () => convert(0, 1));
+  //     input[0].value = input[1].value * rates[select[0].value] / rates[select[1].value];
+  //   });
 }
 getCurrency();
